5c7231bc6a0416f6910f0d0654f051cc5757ec8b
VuDej <drvu47@gmail.com>
2022-04-25 11:22:58 +0200

    aded test for controller Users, updated README.md file and added license file

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -11,7 +11,7 @@ RSpec.describe 'Posts', type: :request do
 
     describe 'GET /show' do
       it 'returns show page' do
-        get '/users/0/posts/1'
+        get '/users/0/posts/show'
         expect(response).to have_http_status(:success)
         expect(response).to render_template(:show)
         expect(response.body).to include('Here is list of all posts for one user')
