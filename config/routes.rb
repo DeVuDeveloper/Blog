d38e965a4d26947db4eab71451841d2fc1cf1f58
VuDej <drvu47@gmail.com>
2022-04-25 07:38:37 +0200

    generated controllers and views and set html in views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'posts/index'
+  get 'posts/show'
+  get 'users/index'
+  get 'users/show'
+  get 'user/index'
+  get 'user/show'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
