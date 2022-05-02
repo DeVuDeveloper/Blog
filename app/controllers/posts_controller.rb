d3aa02b038fb4554dd34e3f572e0431580c8132c
VuDej <drvu47@gmail.com>
2022-05-02 17:28:06 +0200

    added link for creating posts for user, added logic for create posts in posts controller and added route new

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,32 @@
 class PostsController < ApplicationController
   def index
     @user = User.find(params[:user_id])
-    @posts = @user.posts.order(created_at: :desc)
   end
 
   def show
     @user = User.find(params[:user_id])
     @post = @user.posts.find(params[:id])
+  end
+
+  def new
+    @current_user = current_user
+  end
+
+  def create
+    @post = current_user.posts.build(post_params)
+
+    if @post.save
+      flash[:notice] = 'The post have been created successfully.'
+      redirect_to user_path(@post.author_id, @post.id)
+    else
+      flash[:alert] = 'Adding a new post failed.'
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text)
   end
 end
