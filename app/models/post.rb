afbc2cdccf1632e2666963e3296243b890d10fe5
VuDej <drvu47@gmail.com>
2022-05-02 08:55:05 +0200

    added methodes for most recent posts and comments and update post, likes and comments counters

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,15 @@
 class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
-  has_many :likes
+  has_many :likes, dependent: :destroy
+  has_many :comments, dependent: :destroy
+
+  def update_posts_counter
+    @users.each do |user|
+      user.update_column(:posts_counter, user.posts.count) if user.id == author_id
+    end
+  end
+
+  def most_recent_comments
+    comments.order(created_at: :desc).limit(5)
+  end
 end
