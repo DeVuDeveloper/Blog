216cef95cf86c749e6f5c46a6cd32d9b419fce22
VuDej <drvu47@gmail.com>
2022-05-02 11:33:00 +0200

    exercise best-practice, and suggestions from reviewer

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,8 @@ class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :post
 
+  private
+
   @posts.each do |post|
     post.update_column(:comments_counter, post.comments.count) if post.id == post_id
   end
--- app/models/like.rb
+++ app/models/like.rb
@@ -2,6 +2,8 @@ class Like < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :post
 
+  private
+
   @posts.each do |post|
     post.update_column(:likes_counter, post.likes.count) if post.id == post_id
   end
