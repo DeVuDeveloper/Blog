7aa19f1e6b485fa60ca7b66779a684f493d2bd67
VuDej <drvu47@gmail.com>
2022-05-02 21:50:58 +0200

    added method new in post, likes and comments controller

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,4 +1,8 @@
 class LikesController < ApplicationController
+  def new
+    @like = Like.new
+  end
+
   def create
     @post = Post.find(params[:post_id])
     @like = @post.likes.new(author_id: current_user.id, post_id: @post.id)
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,6 +10,7 @@ class PostsController < ApplicationController
   end
 
   def new
+    @post = Post.new
     @current_user = current_user
   end
 
