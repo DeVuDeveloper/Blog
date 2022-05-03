9983e525008603c3deac8f5f14e9e6d377520a81
VuDej <drvu47@gmail.com>
2022-05-03 08:06:15 +0200

    tests for modules done

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   has_many :likes, foreign_key: 'author_id', dependent: :destroy
 
   validates :name, presence: true
-  validates_numericality_of :post_counter, only_integer: true, greater_than_or_equal_to: 0
+  validates_numericality_of :posts_counter, only_integer: true, greater_than_or_equal_to: 0
 
   def most_recent_posts
     posts.order(created_at: :desc).limit(3)
