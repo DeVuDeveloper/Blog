0cd6b80a7e79748ae6ad05115586680c4d64a80a
VuDej <drvu47@gmail.com>
2022-05-09 12:48:26 +0200

    installed gems devise and open letter. Customised signup form, confirmable and reset password working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,18 @@
 class ApplicationController < ActionController::Base
-  def current_user
-    User.find(1)
+  protect_from_forgery with: :exception
+  before_action :authenticate_user!
+
+  before_action :update_allowed_parameters, if: :devise_controller?
+
+  protected
+
+  def update_allowed_parameters
+    devise_parameter_sanitizer.permit(:sign_up) do |u|
+      u.permit(:email, :name, :photo, :bio, :posts_counter, :password, :password_confirmation)
+    end
+
+    devise_parameter_sanitizer.permit(:account_update) do |u|
+      u.permit(:email, :name, :photo, :bio, :password, :password_confirmation, :current_password)
+    end
   end
 end
