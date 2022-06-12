1a5568f2ebca255f34e30515b9dac6a0c949a315
VuDej <drvu47@gmail.com>
2022-06-12 03:34:20 +0200

    fix deployment

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable,
-         :confirmable, :trackable
+         :recoverable, :rememberable, :validatable
 
   has_many :posts, foreign_key: 'author_id', dependent: :destroy
   has_many :comments, foreign_key: 'author_id', dependent: :destroy
