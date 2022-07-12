ecc7857c1001de1c94629d03221321d43266a0fd
VuDej <drvu47@gmail.com>
2022-07-12 17:40:19 +0200

    deploy

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,8 +21,8 @@ Rails.application.configure do
   # config.require_master_key = true
 
   # Disable serving static files from the `/public` folder by default since
-  # Apache or NGINX already handles this.
-  config.public_file_server.enabled = ENV["RAILS_SERVE_STATIC_FILES"].present?
+# Apache or NGINX already handles this.
+config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present? || ENV['RENDER'].present?
 
   # Compress CSS using a preprocessor.
   # config.assets.css_compressor = :sass
